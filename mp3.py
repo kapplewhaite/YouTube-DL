88ee5dee998ad4f20d4c5aceb76d3790ff66eedf
Kaiden A. <69645495+kapplewhaite@users.noreply.github.com>
2024-05-31 11:00:27 -0400

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -2,24 +2,29 @@
 from pytube import YouTube
 import os
 
-# url input from user
-yt = YouTube(
-	str(input("Enter the URL of the video you want to download: \n>> ")))
+while True:
+	# url input from user
+	yt = YouTube(
+		str(input("Enter the URL of the video you want to download: \n>> ")))
 
-# extract only audio
-video = yt.streams.filter(only_audio=True).first()
+	# extract only audio
+	video = yt.streams.filter(only_audio=True).first()
 
-# check for destination to save file
-print("Enter the destination (leave blank for current directory)")
-destination = str(input(">> ")) or 'Downloads'
+	# check for destination to save file
+	print("Enter the destination (leave blank for current directory)")
+	destination = str(input(">> ")) or 'Downloads'
 
-# download the file
-out_file = video.download(output_path=destination)
+	# download the file
+	out_file = video.download(output_path=destination)
 
-# save the file
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
+	# save the file
+	base, ext = os.path.splitext(out_file)
+	new_file = base + '.mp3'
+	os.rename(out_file, new_file)
 
-# result of success
-print(yt.title + " has been successfully downloaded.")
+	# result of success
+	print(yt.title + " has been successfully downloaded.")
+
+	continue_operation = input("Would you like to continue? y/n: ")
+	if continue_operation == "n":
+		break
